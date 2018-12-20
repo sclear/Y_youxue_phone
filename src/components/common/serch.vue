a958b3999d2dda30358dd5eed1755d9e1695b0bf
yq <1392227049@qq.com>
2018-12-20 19:03:45 +0800

    1220

---
+++ src/components/common/serch.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="serch">
+      <div class="icon-z"></div>
+      <div class="serch-input">
+          <input type="text">
+          <div></div>
+      </div>
+      <div class="icon-z"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped >
+.serch{
+    height:1.6875rem;
+    display: flex;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding: auto .6875rem;
+    width:100%;
+    .icon-z{
+        height:1.6875rem;
+        width:1.6875rem;
+        line-height: 1.6875rem;
+        text-align: center;
+        background: burlywood;
+    }
+    .serch-input{
+        width:15.3125rem;
+        height:1.6875rem;
+        input{
+            height:27px !important;
+            width:203px;
+            box-sizing: border-box;
+            // border:1px solid @bgcol;
+            // border-right:1px solid black;
+            outline: none;
+        }
+        div{
+            height:1.6875rem;
+            width:2.625rem;
+            background: red;
+            // float: left;
+            display: inline-block;
+        }
+    }
+}
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+      <!-- <serch></serch> -->
+      
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+      serch: () => import('./../common/serch.vue')
+  }
+};
+</script>
+
+<style lang='less' scoped >
+.home{}
+</style>
